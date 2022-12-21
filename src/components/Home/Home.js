dd62fdf75f5dce347b9894b6c1e948d959d5f404
safil badarudeen <safilbadarudeen@gmail.com>
2022-12-21 17:35:37 +0530

    added async thunk - data not coming to action creator in extra reducer is a bug to fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,40 +1,20 @@
-import React, { useEffect } from 'react'
-import MovieListing from '../MovieListing/MovieListing'
-import movieApi from '../../common/apis/movieApi'
-import {APIkey} from '../../common/apis/MovieApiKey'
-import {useDispatch} from 'react-redux'
-import {addMovies} from '../../features/movies/movieSlice'
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import { useDispatch } from "react-redux";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
-
-
-const Home=()=> {
+const Home = () => {
   const dispatch = useDispatch();
+  useEffect(() => {
+  dispatch(fetchAsyncMovies())  
+  },[dispatch]); // to reload when change happensin dispatch
 
-  useEffect(()=>{
-    const fetchMovies=async()=>{
-      const movieText ="english"
-      const response =await movieApi
-      .get(
-        `?apikey=${APIkey}&s=${movieText}&type=movie`
-      )
-      // console.log(response)
-      
-      .catch((err)=>{
-        console.log("Err : ",err)
-      })
-      
-       dispatch(addMovies(response.data))
-
-    }
-    fetchMovies()
-  },[])
-  
   return (
     <div>
-    <div className='banner-img'></div>
-    <MovieListing/>
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice , createAsyncThunk } from '@reduxjs/toolkit'
+import movieApi from '../../common/apis/movieApi'
+import {APIkey} from '../../common/apis/MovieApiKey'
+
+
+ export const fetchAsyncMovies= createAsyncThunk(
+    'movies/fetchAsyncMovies',async()=>{
+        const movieText = "harry";
+    const response =await movieApi.get(
+      `?apikey=${APIkey}&s=${movieText}&type=movie`
+    )
+    //console.log(response.data)
+   return response.data
+ })
 
 const initialState={
     movies:{}
@@ -10,13 +23,23 @@ const movieSlice = createSlice({
     reducers:{
         addMovies:(state,{payload})=>{
           state.movies=payload    
-        }
+        },
+    extraReducers:{
+     [fetchAsyncMovies.pending]:()=>{
+        console.log("pending")
+     },
+     [fetchAsyncMovies.fulfilled]:(state,{payload})=>{
+        console.log('fullfilled')
+        return {...state,movies:payload}
+     },
+     [fetchAsyncMovies.rejected]:()=>{
+        console.log("rejected")
+     },
+    }
     }
   
 })
 
 export const { addMovies} = movieSlice.actions
 export const getAllMovies=(state)=>state.movies.movies
-
-
 export default movieSlice.reducer
